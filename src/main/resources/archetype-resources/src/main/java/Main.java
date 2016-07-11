578fb77a41793489ffbeb26e8312eccabde62c95
John Freeman <john.freeman.opensource@gmail.com>
2016-07-11 12:06:47 +0100

    Added Main class to archetype
    Also creates main java source directory and package directories.

---
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,11 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
