4de1ded13bcf3967b3e5bbad790a53fde68e82e9
John Freeman <john.freeman.opensource@gmail.com>
2016-09-11 20:46:49 +0100

    Reduced Java file indent to 2 spaces (#3)
    Reduced Java file indent from 4 to 2 spaces.

--- src/main/resources/archetype-resources/src/main/java/Main.java
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -5,7 +5,7 @@ package ${package};
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
 }
