bcda4b3263cf69a42934cee004a573b6c2064602
John Freeman <john.freeman.opensource@gmail.com>
2016-09-16 00:04:05 +0100

    Added logback-classic logger
    Added logback-classic logger (Slf4j implementation).

--- src/main/resources/archetype-resources/src/main/java/Main.java
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -3,12 +3,17 @@
 #set( $symbol_escape = '\' )
 package ${package};
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Description of class.
  */
 public class Main {
 
+  private static final Logger log = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    log.info("Hello, World!");
   }
 }
