609868a82572ba87a06bb1840e62a76e445e57e6
John Freeman <john.freeman.opensource@gmail.com>
2016-09-17 22:19:32 +0100

    Fixed file ownership of Maven build (#42)
    Everything downloaded/written by Docker had root ownership; ownership is now set to the user who runs the build.

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,12 +3,52 @@
 set -e
 
 DOCKER_FILE_DIR="$(dirname "$0")"
+
+USER_ID=$(id -u $USER)
+GROUP_ID=$(id -g $USER)
+GROUP_NAME=$(id -g -n $USER)
+DOCKER_GID=$(getent group docker | cut -d: -f3)
+
+cat <<EOF > $DOCKER_FILE_DIR/Dockerfile
+FROM maven:3.3.9-jdk-8-alpine
+
+RUN apk add --no-cache \\
+		ca-certificates \\
+		curl \\
+		openssl
+
+ENV DOCKER_BUCKET get.docker.com
+ENV DOCKER_VERSION 1.12.1
+ENV DOCKER_SHA256 05ceec7fd937e1416e5dce12b0b6e1c655907d349d52574319a1e875077ccb79
+
+RUN set -x \\
+	&& curl -fSL "https://\${DOCKER_BUCKET}/builds/Linux/x86_64/docker-\${DOCKER_VERSION}.tgz" -o docker.tgz \\
+	&& echo "\${DOCKER_SHA256} *docker.tgz" | sha256sum -c - \\
+	&& tar -xzvf docker.tgz \\
+	&& mv docker/* /usr/local/bin/ \\
+	&& rmdir docker \\
+	&& rm docker.tgz \\
+	&& docker -v
+
+# Delete 'ping' group (999) as it's using the same GID as 'docker' group on Travis CI.
+
+RUN addgroup -g $GROUP_ID $GROUP_NAME \\
+    && adduser -h /home/$USER -D -G $GROUP_NAME -u $USER_ID $USER \\
+    && delgroup ping \\
+    && addgroup -g $DOCKER_GID docker \\
+    && addgroup $USER docker
+
+USER $USER
+
+ENV MAVEN_CONFIG "/home/$USER/.m2"
+EOF
+
 (cd $DOCKER_FILE_DIR && docker build -t mvn-docker .)
 
 mvn() {
     docker run --rm -it \
         -v /var/run/docker.sock:/var/run/docker.sock \
-        -v ~/.m2:/root/.m2 \
+        -v ~/.m2:/home/$USER/.m2 \
         -v "$PWD":/usr/src/maven-build \
         -w /usr/src/maven-build \
         -e "BINTRAY_USER=$BINTRAY_USER" \
