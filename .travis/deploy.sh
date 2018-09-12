19633b12c146ee262d4a3bccb982f1ef0b4f60db
John Freeman <john.freeman.opensource@gmail.com>
2018-09-12 11:31:34 +0000

    Added GPG signatures
    In preparation for deploy to Maven Central.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+set -e
+set +x
+
+# See http://www.debonair.io/post/maven-cd/ for instructions
+openssl aes-256-cbc \
+    -K "$encrypted_458d6d279c1d_key" \
+    -iv "$encrypted_458d6d279c1d_iv" \
+    -in .travis/codesigning.asc.enc \
+    -out .travis/codesigning.asc -d
+gpg --fast-import .travis/codesigning.asc
+
 set -x
 
 ./mvnw deploy \
