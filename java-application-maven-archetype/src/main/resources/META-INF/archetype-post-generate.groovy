4dcbf27f2d641e181ebecbe5576c5ee5a2d9e097
John Freeman <john.freeman.opensource@gmail.com>
2018-09-21 17:34:24 +0000

    Added GantSign code style for IntelliJ
    To ensure the code is well formatted.

--- java-application-maven-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ java-application-maven-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,4 +1,5 @@
 dir = new File(new File(request.outputDirectory), request.artifactId)
+idea = new File(dir, ".idea")
 
 def run(String cmd) {
     def process = cmd.execute([], dir)
@@ -8,8 +9,23 @@ def run(String cmd) {
     }
 }
 
+def writeCodeStyle() {
+    def sb = new StringBuilder()
+    sb.append('<component name="ProjectCodeStyleConfiguration">\n')
+    new URL(request.properties["intellijCodeStyleUrl"]).eachLine {
+        sb.append("  " + it + "\n")
+    }
+    sb.append("</component>")
+    xml = sb.toString()
+    xml = xml.replaceFirst('<code_scheme name="[^"]+"', '<code_scheme name="Project"')
+    def codeStyles = new File((File) idea, "codeStyles")
+    def file = new File(codeStyles, "Project.xml")
+    file.setText(xml, "UTF-8")
+}
+
 run("mvn -N io.takari:maven:wrapper")
 run("./mvnw sortpom:sort")
 run("./mvnw license:update-file-header@config")
 run("./mvnw license:update-file-header@sources")
 run("./mvnw fmt:format")
+writeCodeStyle()
