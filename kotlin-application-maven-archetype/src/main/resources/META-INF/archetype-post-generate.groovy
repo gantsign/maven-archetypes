fc571441e1efacb859e0e00d6581d1c6ee746df9
John Freeman <john.freeman.opensource@gmail.com>
2018-09-21 18:06:29 +0000

    Added GantSign inspection profile for IntelliJ
    To encourage best practice.

--- kotlin-application-maven-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ kotlin-application-maven-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -23,9 +23,34 @@ def writeCodeStyle() {
     file.setText(xml, "UTF-8")
 }
 
+def writeInspectionProfile() {
+    def sb = new StringBuilder()
+    sb.append('<component name="InspectionProjectProfileManager">\n')
+    def url = request.properties["intellijInspectionProfileUrl"]
+    new URL(url).eachLine {
+        sb.append("  " + it + "\n")
+    }
+    sb.append("</component>")
+    xml = sb.toString()
+    def m = (~/<option name="myName" value="([^"]+)"/).matcher(xml)
+    if (!m.find()) {
+        throw new RuntimeException("Unable to find inspection profile name in " + url)
+    }
+    def name = m.group(1)
+
+    def inspectionProfiles = new File((File) idea, "inspectionProfiles")
+    def file = new File(inspectionProfiles, name + ".xml")
+    file.setText(xml, "UTF-8")
+
+    def settings = new File(inspectionProfiles, "profiles_settings.xml")
+    def contents = settings.getText("UTF-8").replaceFirst("@intellijInspectionProfileName@", name)
+    settings.setText(contents, "UTF-8")
+}
+
 run("mvn -N io.takari:maven:wrapper")
 run("./mvnw sortpom:sort")
 run("./mvnw license:update-file-header@config")
 run("./mvnw license:update-file-header@sources")
 run("./mvnw ktlint:format")
 writeCodeStyle()
+writeInspectionProfile()
