b0e91631473ed0ab351b7c3419da274331c988a4
John Freeman <john.freeman.opensource@gmail.com>
2021-01-02 20:29:26 +0000

    Fixed Maven Site deploy (#445)
    Maven settings path.

--- .github/scripts/deploy-site.sh
+++ .github/scripts/deploy-site.sh
@@ -18,7 +18,7 @@ set -x
 
 ./mvnw scm-publish:publish-scm \
     -P gh-pages \
-    --settings .travis/settings.xml \
+    --settings .github/scripts/settings.xml \
     -Darchetype.test.skip=true \
     "-Drevision=${GIT_TAG:-development-SNAPSHOT}" \
     --batch-mode
